975010bd2cfa6ec082e2069cbea2953a882267d5
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2014-09-09 10:21:45 +0200

    initialer commit

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "clearcacheextend".
+#
+# Auto generated 19-09-2012 17:26
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Extended TCEMAIN.clearCacheCmd command',
+	'description' => 'Extend TCEMAIN.clearCacheCmd width some userfull options like using of aliases, clearing whole branches, clearing pages on the ground of their content etc.',
+	'category' => 'be',
+	'shy' => 0,
+	'version' => '0.1.5',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Juraj Sulek',
+	'author_email' => 'juraj@sulek.sk',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"6934";s:29:"class.tx_clearcacheextend.php";s:4:"8d54";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"b16e";s:13:"locallang.php";s:4:"0a9e";s:10:"README.txt";s:4:"9fa9";s:14:"doc/manual.sxw";s:4:"4e1f";s:19:"doc/wizard_form.dat";s:4:"887e";s:20:"doc/wizard_form.html";s:4:"ffd7";}',
+);
+
+?>
---
+++ ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
+
+$TYPO3_CONF_VARS['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][]='EXT:clearcacheextend/class.tx_clearcacheextend.php:&tx_clearcacheextend->clear_cacheCmdExtend';
+
+?>
