73624272d1ea81c090f10f596a73a1264fa572a8
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2014-09-09 11:45:46 +0200

    Version 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,21 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "clearcacheextend".
-#
-# Auto generated 19-09-2012 17:26
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "clearcacheextend".
+ *
+ * Auto generated 09-09-2014 11:45
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Extended TCEMAIN.clearCacheCmd command',
 	'description' => 'Extend TCEMAIN.clearCacheCmd width some userfull options like using of aliases, clearing whole branches, clearing pages on the ground of their content etc.',
 	'category' => 'be',
 	'shy' => 0,
-	'version' => '0.1.5',
+	'version' => '1.0.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -41,7 +41,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"6934";s:29:"class.tx_clearcacheextend.php";s:4:"8d54";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"b16e";s:13:"locallang.php";s:4:"0a9e";s:10:"README.txt";s:4:"9fa9";s:14:"doc/manual.sxw";s:4:"4e1f";s:19:"doc/wizard_form.dat";s:4:"887e";s:20:"doc/wizard_form.html";s:4:"ffd7";}',
+	'_md5_values_when_last_written' => 'a:9:{s:9:"ChangeLog";s:4:"6934";s:29:"class.tx_clearcacheextend.php";s:4:"c490";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"b16e";s:13:"locallang.php";s:4:"0a9e";s:10:"README.txt";s:4:"9fa9";s:14:"doc/manual.sxw";s:4:"4e1f";s:19:"doc/wizard_form.dat";s:4:"887e";s:20:"doc/wizard_form.html";s:4:"ffd7";}',
+	'suggests' => array(
+	),
 );
 
 ?>
